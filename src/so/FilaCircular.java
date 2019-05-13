4cdad4cd51a35ad38467190d70e7fd53e31e491e
arthurteixeira <arthurtjardim@gmail.com>
2019-05-13 15:01:42 -0300

    semaforo removido

--- src/so/FilaCircular.java
+++ src/so/FilaCircular.java
@@ -11,9 +11,6 @@ public class FilaCircular {
     private int inicio;
     private int fim;
     private int numeroElementos;
-    private Semaphore empty;
-    private Semaphore full;
-    private Semaphore mutex;
     private int idReq = 0;
     private int maximoReq;
     private int minimoReq;
@@ -23,9 +20,6 @@ public class FilaCircular {
         this.inicio = 0;
         this.fim = 0;
         this.numeroElementos = 0;
-        this.empty = new Semaphore(tamanho);
-        this.full = new Semaphore(0);
-        this.mutex = new Semaphore(1);
         this.maximoReq = maximo;
         this.minimoReq = minimo;
         //Gera todas as requisições iniciais
@@ -44,28 +38,10 @@ public class FilaCircular {
     
     public void addElemento(Requisicao objeto){
         if(!filaCheia()){
-            
-            try {
-                empty.acquire();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(FilaCircular.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
             fila[fim++] = objeto;
-            
-            try {
-                mutex.acquire();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(FilaCircular.class.getName()).log(Level.SEVERE, null, ex);
-            }
             numeroElementos++;
-            mutex.release();
-            
             if(fim == fila.length)
                 fim = 0;
-            
-            full.release();
-            
             System.out.println("Requisição entrou no vetor de requisições.");
         }else{
             System.out.println("Requisição NÃO entrou no vetor de requisições.");
@@ -74,27 +50,11 @@ public class FilaCircular {
     
     public Requisicao removerElemento(){
         if(!filaVazia()){
-            try {
-                full.acquire();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(FilaCircular.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
             Requisicao requisicao = fila[inicio++];
             if(inicio == fila.length){
                inicio = 0; 
             }
-            
-            try {
-                mutex.acquire();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(FilaCircular.class.getName()).log(Level.SEVERE, null, ex);
-            }
             numeroElementos--;
-            mutex.release();
-            
-            empty.release();
-            
             addElemento(gerarRequisicao()); //Quando remove uma gera outra requisição
             return requisicao;
         }else{
